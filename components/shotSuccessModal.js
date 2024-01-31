c37ed15bed1d231c6433b146608aa526c9d87bfd
Zahrd <cadenleiscool@gmail.com>
2024-01-30 18:05:45 -0800

    Fix shotSuccessModal, but still needs sizing fixing

--- components/shotSuccessModal.js
+++ components/shotSuccessModal.js
@@ -6,24 +6,24 @@ function ShotSuccessModal(props) {
   const matchData = JSON.parse(JSON.stringify(props.eventReducer.currentMatchData));
 
   return(
-    <Modal transparent={true} visible={props.modalVisible} onRequestClose={() => {
-      props.setModalVisible(!props.modalVisible);
+    <Modal style={{ width: 100, height: 40 }} transparent={true} visible={props.shotModalVisible} onRequestClose={() => {
+      props.setShotModalVisible(!props.shotModalVisible);
     }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <View style={{ margin: 20, backgroundColor: 'white', padding: 35, borderRadius: 20, alignItems: 'center' }}>
 
           <TouchableOpacity style={{ backgroundColor: '#e83149', height: 30, width: 30, borderRadius: 40, alignSelf: 'flex-start', justifyContent: 'center' }} 
-          onPress={() => { props.setModalVisible(!props.modalVisible) }}>
-            <Text style={{ alignSelf: 'center', color: 'white', fontSize: 15 }}>x</Text>
+          onPress={() => { props.setShotModalVisible(!props.shotModalVisible) }}>
+            <Text style={{ alignSelf: 'center', color: 'white', fontSize: 15 }}>X</Text>
           </TouchableOpacity>
           {/* Button to close modal */}
 
           <Image style={{ height: 100, width: 100, marginBottom: 10 }} source={require('../assets/game_pieces/note.png')} />
 
-          <View style={{ flexDirection: 'column' }}>
+          <View style={{ flexDirection: 'column', alignItems: 'stretch' }}>
 
-            <TouchableOpacity style={{ backgroundColor: '#85e882', borderRadius: 10, padding: 10, marginHorizontal: 20, marginBottom: 25, width: '45%', height: '100%' }} onPress={() => {
-              console.log('Successful shot button in modal pressed');
+            <TouchableOpacity style={{ backgroundColor: '#85e882', borderRadius: 10, padding: 10, marginHorizontal: 20, marginBottom: 25 }} onPress={() => {
+              //console.log('Successful shot button in modal pressed');
 
               let temp = (props.matchPhase == 'auto') ? props.autoActions : props.teleopActions;
               temp.push(String(props.matchPhase + props.modalType));
@@ -34,8 +34,8 @@ function ShotSuccessModal(props) {
               props.setShotModalVisible(!props.shotModalVisible);
             }}><Text style={{ alignSelf: 'center' }}>Successful Shot</Text></TouchableOpacity>
 
-            <TouchableOpacity style={{ backgroundColor: '#f56c6c', borderRadius: 10, padding: 10, marginHorizontal: 20, marginBottom: 25, width: '45%', height: '100%' }} onPress={() => {
-              console.log('Failed shot button in modal pressed');
+            <TouchableOpacity style={{ backgroundColor: '#f56c6c', borderRadius: 10, padding: 10, marginHorizontal: 20, marginBottom: 25 }} onPress={() => {
+              //console.log('Failed shot button in modal pressed');
 
               let temp = (props.matchPhase == 'auto') ? props.autoActions : props.teleopActions; 
               temp.push(String(props.matchPhase + 'Failed' + props.modalType));
